2d4664e05ab0022b5c8f5b6c4eac66974c188f62
Aldair Avelino <aldair03avelino@gmail.com>
2023-10-25 13:03:13 +0100

    Error page for sign up - AA

---
+++ src/pages/error/sign-up-error.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Error</title>
+  <link rel="stylesheet" href="../../css/sign-up-error.css">
+
+</head>
+
+<body>
+  <div class="cont_principal">
+    <div class="cont_error">
+      <h1>Oops</h1>
+      <p>There was an issue with your submission</p>
+      <p>Please check the data and try again</p>
+      <a href="../auth/sign-up.php" class="btn">Try Again</a>
+    </div>
+    <div class="cont_aura_1"></div>
+    <div class="cont_aura_2"></div>
+  </div>
+  <script src="../../js/auth-error.js"></script>
+
+</body>
+
+</html>
