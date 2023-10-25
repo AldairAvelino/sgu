a95aea8304f633efca76ce612b171a1e066f4312
Aldair Avelino <aldair03avelino@gmail.com>
2023-10-25 10:05:34 +0100

    Page responsible for Login - AA

---
+++ src/pages/auth/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Create User</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./auth.css">
+</head>
+
+<body>
+  <form id="regForm" action="" method="post">
+    <h1>Login</h1>
+    
+    <div>
+      <p><input placeholder="Insert Your E-mail Address" type="email" name="E-mail"></p>
+      <p><input placeholder="Insert Your Password" type="password" name="password"></p>
+      <button class="btn" type="submit" name="login">Login</button>
+    </div>
+  </form>
+</body>
+
+</html>
