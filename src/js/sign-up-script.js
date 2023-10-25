309abcc6cb2943ef4fb01ed230a6c3723fb087da
Aldair Avelino <aldair03avelino@gmail.com>
2023-10-25 10:10:53 +0100

    Added commentaries in code - AA

--- src/js/sign-up-script.js
+++ src/js/sign-up-script.js
@@ -6,33 +6,33 @@ function showTab(n) {
   x[n].style.display = "block";
 
   if (n === 0) {
-    document.getElementById("prevBtn").style.display = "none";
+    document.getElementById("prevBtn").style.display = "none"; // Esconde o botão "Previous" na primeira etapa
   } else {
-    document.getElementById("prevBtn").style.display = "inline";
+    document.getElementById("prevBtn").style.display = "inline"; // Mostra o botão "Previous" nas etapas subsequentes
   }
   if (n === (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Sign Up";
+    document.getElementById("nextBtn").innerHTML = "Sign Up"; // Altera o texto do botão "Next" para "Sign Up" na última etapa
   } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
+    document.getElementById("nextBtn").innerHTML = "Next"; // Restaura o texto do botão "Next" nas etapas anteriores
   }
 
-  fixStepIndicator(n);
+  fixStepIndicator(n); // Atualiza os indicadores de etapa
 }
 
 function nextPrev(n) {
   const x = document.getElementsByClassName("tab");
 
-  if (n === 1 && !validateForm()) return false;
+  if (n === 1 && !validateForm()) return false; // Valida o formulário; se for inválido, interrompe o progresso
 
-  x[currentTab].style.display = "none";
-  currentTab = currentTab + n;
+  x[currentTab].style.display = "none"; // Esconde a etapa atual
+  currentTab = currentTab + n; // Atualiza a aba atual
 
   if (currentTab >= x.length) {
-    document.getElementById("submitButton").click();
+    document.getElementById("submitButton").click(); // Clica no botão de envio oculto quando todas as etapas forem concluídas
     return false;
   }
 
-  showTab(currentTab);
+  showTab(currentTab); // Exibe a próxima etapa
 }
 
 function validateForm() {
@@ -45,16 +45,16 @@ function validateForm() {
 
   for (i = 0; i < y.length; i++) {
     if (y[i].value === "") {
-      y[i].className += " invalid";
-      valid = false;
+      y[i].className += " invalid"; // Adiciona a classe "invalid" aos campos vazios
+      valid = false; // Define a validação como falsa se houver campos vazios
     }
   }
 
   if (valid) {
-    document.getElementsByClassName("step")[currentTab].className += " finish";
+    document.getElementsByClassName("step")[currentTab].className += " finish"; // Marca a etapa como concluída se for válida
   }
 
-  return valid;
+  return valid; // Retorna o status de validação
 }
 
 function fixStepIndicator(n) {
@@ -62,8 +62,8 @@ function fixStepIndicator(n) {
   const x = document.getElementsByClassName("step");
 
   for (i = 0; i < x.length; i++) {
-    x[i].className = x[i].className.replace(" active", "");
+    x[i].className = x[i].className.replace(" active", ""); // Remove a classe "active" de todos os indicadores de etapa
   }
 
-  x[n].className += " active";
+  x[n].className += " active"; // Adiciona a classe "active" ao indicador de etapa atual
 }
