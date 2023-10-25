03c6f695170dda76d3b34ed92b3318d8480293c7
Aldair Avelino <aldair03avelino@gmail.com>
2023-10-25 13:02:51 +0100

    Error page for login - AA

---
+++ src/pages/error/login-error.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Login Error</title>
+  <link rel="stylesheet" href="../../css/login-error.css">
+
+</head>
+
+<body>
+  <div class="cont_principal">
+    <div class="cont_error">
+      <h1>Oops</h1>
+      <p>Your Login Failed</p>
+      <p>Please check your email and password and try again</p>
+      <a href="../auth/login.php" class="btn">Login Again</a>
+    </div>
+    <div class="cont_aura_1"></div>
+    <div class="cont_aura_2"></div>
+  </div>
+  <script src="../../js/auth-error.js"></script>
+
+</body>
+
+</html>
