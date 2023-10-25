90e8879b823f7fdfef18d0043f9d0758ba598eac
Aldair Avelino <aldair03avelino@gmail.com>
2023-10-25 17:50:11 +0100

    Changes over layout - AA

---
+++ src/pages/success/user-deleted.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Success</title>
+  <link rel="stylesheet" href="../../css/sign-up-success.css">
+
+</head>
+
+<body>
+  <div class="cont_principal">
+    <div class="cont_error">
+      <h1>Bye Bye!</h1>
+      <p>User Deleted</p>
+      <a href="../auth/sign-up.php" class="btn">Create New Account</a>
+    </div>
+    <div class="cont_aura_1"></div>
+    <div class="cont_aura_2"></div>
+  </div>
+  <script src="../../js/auth-error.js"></script>
+
+</body>
+
+</html>
---
+++ src/pages/success/user-details-updated.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Success</title>
+  <link rel="stylesheet" href="../../css/sign-up-success.css">
+
+</head>
+
+<body>
+  <div class="cont_principal">
+    <div class="cont_error">
+      <h1>Well Done!</h1>
+      <p>Congratulations!</p>
+      <p>User Details Updated</p>
+      <a href="../../../user.php" class="btn">See Changes</a>
+    </div>
+    <div class="cont_aura_1"></div>
+    <div class="cont_aura_2"></div>
+  </div>
+  <script src="../../js/auth-error.js"></script>
+
+</body>
+
+</html>
