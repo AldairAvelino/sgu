045dbe999afb98fc481e1d61ff8c99b06126e79c
Aldair Avelino <aldair03avelino@gmail.com>
2023-10-25 17:51:08 +0100

    Layout channges - AA

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="src/css/menu.css">
+</head>
+
+<body>
+    <header>
+        <p id="header">SGU</p>
+        <div class="black-out"></div>
+        <p class="menu">Hover Here for the Menu</p>
+        <nav>
+            <ul>
+                <li><a href="src/pages/auth/login.php">Login</a></li>
+                <li><a href="src/pages/auth/sign-up.php">Sign Up</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
