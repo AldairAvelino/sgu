8b3f0c78610f70c6c237d4ab2b58fe9146fb50eb
Aldair Avelino <aldair03avelino@gmail.com>
2023-10-25 13:02:17 +0100

    Success file for sign up - AA

---
+++ src/pages/success/sign-up-success.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Success</title>
+  <link rel="stylesheet" href="../../css/sign-up-success.css">
+
+</head>
+
+<body>
+  <div class="cont_principal">
+    <div class="cont_error">
+      <h1>Well Done!</h1>
+      <p>Congratulations!</p>
+      <p>Account created successfully.</p>
+      <a href="../auth/login.php" class="btn">Welcome Aboard!</a>
+    </div>
+    <div class="cont_aura_1"></div>
+    <div class="cont_aura_2"></div>
+  </div>
+  <script src="../../js/auth-error.js"></script>
+
+</body>
+
+</html>
